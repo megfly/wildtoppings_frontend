437f5e3d8111b4e95ead44803458c193dcc52d57
megfly <mflynn333@gmail.com>
2020-10-27 16:17:33 -0400

    worked on adding pizzas to the DOM

--- src/index.js
+++ src/index.js
@@ -32,18 +32,21 @@ function fetchToppings() {
 const addPizzas = pizzaArr => {
     pizzaArr.data.forEach(pizza => {
         const div = document.createElement("div")
-        const p = document.createElement("p")
+        const pTagForTitle = document.createElement("p")
+        const pTagForDescription = document.createElement("p")
         const button = document.createElement("button")
         const ul = document.createElement("ul")
 
         div.setAttribute("class", "card")
-        div.setAttribute("data-id", pizzaArr.id)
+        div.setAttribute("data-id", pizza.id)
 
-        p.innerText = pizza.attributes.title
-
-        div.appendChild(p)
-        div.appendChild(button)
+        pTagForTitle.innerText = pizza.attributes.title
+        pTagForDescription.innerText = pizza.attributes.description
+        
+        div.appendChild(pTagForTitle)
+        div.appendChild(pTagForDescription)
         div.appendChild(ul)
+        div.appendChild(button)
         
         const pizzaContainer = document.querySelector(".pizza-container")
         pizzaContainer.appendChild(div)

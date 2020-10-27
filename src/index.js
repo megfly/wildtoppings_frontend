fabb5329d0f552a639f31815b644ea12b8548fa6
megfly <mflynn333@gmail.com>
2020-10-27 15:42:39 -0400

    working on adding pizzas to the dom

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ const TOPPINGS_URL = `${BASE_URL}/api/v1/toppings`
 
 // DOMContent Loaded
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("PIIIZZZZAAAAAA")
-
+    console.log("loaded")
     fetchPizzas()
     fetchToppings()
-
 })
 
 // Fetch Requests
@@ -17,14 +15,37 @@ function fetchPizzas() {
     fetch(PIZZAS_URL) //promise
     .then((response) => response.json())
     .then((pizzaJson) => {
-        console.log(pizzaJson)
+        let pizzaArray = pizzaJson
+        addPizzas(pizzaArray)
     })
-}
+};
 
 function fetchToppings() {
     fetch(TOPPINGS_URL) //promise
     .then((response) => response.json())
     .then((toppingJson) => {
-        console.log(toppingJson)
+        //console.log(toppingJson)
+    })
+};
+
+//Adding objects to the DOM
+const addPizzas = pizzaArr => {
+    pizzaArr.data.forEach(pizza => {
+        const div = document.createElement("div")
+        const p = document.createElement("p")
+        const button = document.createElement("button")
+        const ul = document.createElement("ul")
+
+        div.setAttribute("class", "card")
+        div.setAttribute("data-id", pizzaArr.id)
+
+        p.innerText = pizza.attributes.title
+
+        div.appendChild(p)
+        div.appendChild(button)
+        div.appendChild(ul)
+        
+        const pizzaContainer = document.querySelector(".pizza-container")
+        pizzaContainer.appendChild(div)
     })
 }

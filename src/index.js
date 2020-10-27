89cdf330493d9e48ace38adb1cedccafadd26977
megfly <mflynn333@gmail.com>
2020-10-27 14:35:09 -0400

    created fetch requests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 // URL's
 const BASE_URL = "http://localhost:3000"
-const PIZZAS_URL = `${BASE_URL}/pizzas`
-const TOPPINGS_URL = `${BASE_URL}/toppings`
+const PIZZAS_URL = `${BASE_URL}/api/v1/pizzas`
+const TOPPINGS_URL = `${BASE_URL}/api/v1/toppings`
 
 // DOMContent Loaded
 document.addEventListener('DOMContentLoaded', () => {
     console.log("PIIIZZZZAAAAAA")
+
+    fetchPizzas()
+    fetchToppings()
+
 })
 
 // Fetch Requests
+function fetchPizzas() {
+    fetch(PIZZAS_URL) //promise
+    .then((response) => response.json())
+    .then((pizzaJson) => {
+        console.log(pizzaJson)
+    })
+}
+
+function fetchToppings() {
+    fetch(TOPPINGS_URL) //promise
+    .then((response) => response.json())
+    .then((toppingJson) => {
+        console.log(toppingJson)
+    })
+    .catch()
+}

17dce253b903f32d384923bb862a627afa92ea3e
megfly <mflynn333@gmail.com>
2020-10-27 14:13:18 -0400

    created const variables for urls tothe backend

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// URL's
+const BASE_URL = "http://localhost:3000"
+const PIZZAS_URL = `${BASE_URL}/pizzas`
+const TOPPINGS_URL = `${BASE_URL}/toppings`
+
+// DOMContent Loaded
 document.addEventListener('DOMContentLoaded', () => {
     console.log("PIIIZZZZAAAAAA")
 })
+
+// Fetch Requests

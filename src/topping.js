e30c8710ad5d5d6ef4e23b348df8203c9a58bc51
megfly <mflynn333@gmail.com>
2020-10-29 16:48:37 -0400

    added this.pizza_id

--- src/topping.js
+++ src/topping.js
@@ -2,8 +2,8 @@ class Topping {
     constructor(topping) {
         Topping.all = []
         this.id = topping.id 
+        this.pizza_id = topping.pizza_id
         this.ingredient_name = topping.ingredient_name
-        //console.log(topping.ingredient_name)
         Topping.all.push(this)
     }
 }

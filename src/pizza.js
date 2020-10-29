9659b2c7c536d13d6afc8429a71f7217a6c417ab
megfly <mflynn333@gmail.com>
2020-10-29 15:22:20 -0400

    put in a comment

--- src/pizza.js
+++ src/pizza.js
@@ -10,22 +10,22 @@ class Pizza {
     }
 
     renderPizzaCard() {
-        return `
-        <div class="pizza-card-${this.id}">
-        <br>
+            return `
+            <div class="pizza-card-${this.id}">
+            <br>
 
-        <div data-id=${this.id}
-            <h2 class="card-title">${this.title}</h2>
-                <p class="card-text">${this.description}</p>
+            <div data-id=${this.id}
+                <h2 class="card-title">${this.title}</h2>
+                    <p class="card-text">${this.description}</p>
 
-                <button id="delete" data-id="${this.id}">Delete Pizza!</button>
-                <button id="add-topping" data-id="${this.id}">Add Topping!</button>
+                    <button id="add-topping" data-id="${this.id}">Add Topping!</button>
 
-        <ul id="pizza-${this.id}-toppings">
-        </ul>
+            <ul id="pizza-${this.id}-toppings">
+            </ul>
 
-        </div>
-        `
+            </div>
+            `
+        //EVENT LISTENER TO ADD PIZZA TOPPING TO PIZZA
     }
 
 }

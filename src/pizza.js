704773a4f9158836200f8bc81f100ab88489be13
megfly <mflynn333@gmail.com>
2020-10-29 14:41:24 -0400

    made delete pizza and add topping buttons

--- src/pizza.js
+++ src/pizza.js
@@ -15,12 +15,15 @@ class Pizza {
         <br>
 
         <div data-id=${this.id}
-        <h3 class="card-title">${this.title}</h3>
-            <p class="card-text">${this.description}</p>
+            <h2 class="card-title">${this.title}</h2>
+                <p class="card-text">${this.description}</p>
+
+                <button id="delete" data-id="${this.id}">Delete Pizza!</button>
+                <button id="add-topping" data-id="${this.id}">Add Topping!</button>
 
         <ul id="pizza-${this.id}-toppings">
-         
         </ul>
+
         </div>
         `
     }

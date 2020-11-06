b344d5964a835e2da5bc033a6272694303330fb5
megfly <mflynn333@gmail.com>
2020-11-06 18:48:14 -0500

    modifiying bootstrap styling

--- src/pizza.js
+++ src/pizza.js
@@ -13,24 +13,26 @@ class Pizza {
     renderPizzaCard() {
         //debugger
         return `
+        <div class="row">
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
+
             <div class="pizza-card-${this.id}">
 
-            <img src="https://i.pinimg.com/originals/8d/6c/13/8d6c130c50e948391ba1a2bb1bf8a953.jpg" width="100%" height="100%">
-            <rect width="100%" height="100%" fill="#55595c"/>
+                        <img src="https://i.pinimg.com/originals/8d/6c/13/8d6c130c50e948391ba1a2bb1bf8a953.jpg" width="100%" height="100%">
+                        <rect width="100%" height="100%" fill="#55595c"/>
             
             <div class="card-body">
 
-            <h5 class="card-title" data-id=${this.id}>${this.title}</h5>
+                        <h5 class="card-title" data-id=${this.id}>${this.title}</h5>
 
-              <p class="card-text" data-id=${this.id}>${this.description}</p>
+                        <p class="card-text" data-id=${this.id}>${this.description}</p>
 
-              <ul class="list-group" id="pizza-${this.id}-toppings"> </ul>
+                        <ul class="list-group" id="pizza-${this.id}-toppings"> </ul>
 
               <div class="d-flex justify-content-between align-items-center">
                     
-                
+                </div>
                 </div>
               </div>
             </div>

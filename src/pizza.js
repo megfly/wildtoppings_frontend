aaef9a81979ac86bcbb053f0c41386c6a3733751
megfly <mflynn333@gmail.com>
2020-11-06 18:31:41 -0500

    edited the pizzacard

--- src/pizza.js
+++ src/pizza.js
@@ -13,11 +13,9 @@ class Pizza {
     renderPizzaCard() {
         //debugger
         return `
-        
-    <div class="pizza-card-${this.id}">
-    <div class="row">
         <div class="col-md-4">
           <div class="card mb-4 shadow-sm">
+            <div class="pizza-card-${this.id}">
 
             <img src="https://i.pinimg.com/originals/8d/6c/13/8d6c130c50e948391ba1a2bb1bf8a953.jpg" width="100%" height="100%">
             <rect width="100%" height="100%" fill="#55595c"/>
@@ -31,6 +29,7 @@ class Pizza {
               <ul class="list-group" id="pizza-${this.id}-toppings"> </ul>
 
               <div class="d-flex justify-content-between align-items-center">
+                    
                 
                 </div>
               </div>
@@ -40,4 +39,19 @@ class Pizza {
             `
     }
 
+//     <div class="col-md-4">
+//     <div class="card mb-4 shadow-sm">
+//       <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+//       <div class="card-body">
+//         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+//         <div class="d-flex justify-content-between align-items-center">
+//           <div class="btn-group">
+//             <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+//             <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+//           </div>
+//           <small class="text-muted">9 mins</small>
+//         </div>
+//       </div>
+//     </div>
+//   </div> 
 }

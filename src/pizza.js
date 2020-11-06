c06e35fbb3d0e4ee9015fd5febc9d659361b86b6
megfly <mflynn333@gmail.com>
2020-11-06 15:26:26 -0500

    styled my renderpizzacard with bootstrap

--- src/pizza.js
+++ src/pizza.js
@@ -13,22 +13,34 @@ class Pizza {
     renderPizzaCard() {
         //debugger
         return `
-            <div class="pizza-card-${this.id}">
-            <br>
+    <div class="pizza-card-${this.id}">
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
 
-           
-                <h2 class="card-title" data-id=${this.id}>${this.title}</h2>
-                    <p class="card-text" data-id=${this.id}>${this.description}</p>
+            <img src="https://i.pinimg.com/originals/8d/6c/13/8d6c130c50e948391ba1a2bb1bf8a953.jpg" width="100%" height="100%">
+            <rect width="100%" height="100%" fill="#55595c"/>
+            
+            <div class="card-body">
 
+            <h5 class="card-title" data-id=${this.id}>${this.title}</h5>
 
+              <p class="card-text" data-id=${this.id}>${this.description}</p>
 
-            <ul id="pizza-${this.id}-toppings">
-            </ul>
+              <ul class="list-group" id="pizza-${this.id}-toppings"> </ul>
 
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
             </div>
+          </div>
+        </div>
             `
     }
 
-    //  <div data-id=${this.id}>
-
 }
+
+//notes from the bootsrtap card 
+
+//<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+//<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
